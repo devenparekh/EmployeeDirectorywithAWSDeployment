40447140a48ac3d10494275e675403cd87847e59
devenparekh <devenparekh7@gmail.com>
2022-07-06 15:56:19 -0500

    made a change to directly head to List of students after Login

--- src/main/java/com/example/demoapplication/student/WebSecurityConfig/MvcConfig.java
+++ src/main/java/com/example/demoapplication/student/WebSecurityConfig/MvcConfig.java
@@ -11,6 +11,6 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/s1/api/v1/student").setViewName("studentList");
     }
 }
