9b7db07bba145bdad502346fb06969219824e4b3
devenparekh <devenparekh7@gmail.com>
2023-07-04 14:54:15 -0400

    changed the /newUser controller for validation.

--- src/main/java/com/example/EmployeeDirectory/UserDetails/UserDetailsController/UserDetailsController.java
+++ src/main/java/com/example/EmployeeDirectory/UserDetails/UserDetailsController/UserDetailsController.java
@@ -3,13 +3,10 @@ package com.example.EmployeeDirectory.UserDetails.UserDetailsController;
 import com.example.EmployeeDirectory.UserDetails.UserDetailsEntity.UserInfo;
 import com.example.EmployeeDirectory.UserDetails.UserDetailsService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.ws.rs.core.Response;
 
 @RestController
 public class UserDetailsController {
@@ -22,7 +19,7 @@ public class UserDetailsController {
         if(userInfo == null){
             return ResponseEntity.notFound().build();
         }else {
-            String addedUser = service.addUser(userInfo);
+            service.addUser(userInfo);
             return ResponseEntity.ok("New User Added with Details: " + userInfo);
         }
     }
